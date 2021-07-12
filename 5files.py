02d084282f65ba4856e9d818377f9601f0c36daa
abdsaeed92 <rahmansaeed1992@gmail.com>
2021-07-13 00:09:18 +0300

    sum 5 files

---
+++ 5files.py
@@ -0,0 +1,5 @@
+#This example saying if you have 5 files in it,Each file has  a diferent size: 2048,4357,97658,125,8.Then add each file one by one as a total.Ans calculate the average.output a message saying"The average size is: remumber to use the str()function.
+total = 2048 + 4357 + 97658 + 125 + 8
+files = 5
+average = total/files 
+print("The average size is :"+str(average))
