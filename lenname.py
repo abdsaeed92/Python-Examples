2e69035a14872e02744ceda427becc77d4e36b69
abdsaeed92 <rahmansaeed1992@gmail.com>
2021-07-14 01:22:06 +0300

    calculate the lenthg of character of names multiple any number

---
+++ lenname.py
@@ -0,0 +1,8 @@
+# This program doing calculate the lenthg of character of names multiple any number..
+name = "Ali"
+number = len (name) *9
+print ("Hello "+name+ " your number is "+str(number))
+
+name = "Hala"
+number = len (name) *5
+print ("Hello "+name+ " your number is "+str(number))
