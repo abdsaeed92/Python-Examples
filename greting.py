a03c1f32948067055ce2ea75da7f297228d916e3
abdsaeed92 <rahmansaeed1992@gmail.com>
2021-07-14 01:09:58 +0300

    insert name and department by greeting fun..

---
+++ greting.py
@@ -0,0 +1,6 @@
+# This program is help U to input the name for the stuff and his department.
+
+def greeting(name,department):
+    print("welcom,"+name)
+    print("you are port of,"+department)
+greeting ("Abdulrahman","DevOps")
